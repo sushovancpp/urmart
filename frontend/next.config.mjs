d131e21c93593f951decc21dcd9db6e333294e07
sushovancpp <masantasushovan@gmail.com>
2026-02-23 22:33:18 +0530

    Fix path alias resolution for Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,10 +3,15 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
       },
     ];
+  },
+
+  webpack: (config) => {
+    config.resolve.alias["@"] = __dirname;
+    return config;
   },
 };
 
